540a148c573b560eaf7704456f8a4d06ba737100
KevinSamosir <153571897+KevinnSamosir@users.noreply.github.com>
2024-02-06 21:34:37 +0700

    Tugas

--- t03_01.c
+++ t03_01.c
@@ -1,11 +1,33 @@
-// NIM - NAMA
+// 12S23015 - Kevin Kristoforus Samosir
 
 #include <stdio.h>
 
 int main(int _argc, char **_argv)
-{
+ {
+  int m,i,j;
+       
 
-  // your code here
+  scanf("%d",&m);
+
+  int n [m];
+
+  for (i=0; i<m; i++) {
+    scanf("%d",&n[i]);
+  }
+  
+  for (int j = i + 1; j < m; j++) {
+   
+  }for (i=0; i<m; i++) {
+    for(j=i+1; j<m; j++) {
+      if (n[i] > n[j]) {
+        int temp = n[i];
+        n[i] = n[j];
+        n[j] = temp;
+      }
+    }
+  }
+  printf("%d\n",n[0]);
+  printf("%d",n[m-1]);
 
   return 0;
-}
+ }
